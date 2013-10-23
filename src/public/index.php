d84225c6585f20b693d378e4f1cca95458db50ea
Jacek Kobus <kobus.jacek@gmail.com>
2013-10-23 15:12:48 +0200

    updated license to new bsd 3 added test suite new constants - app env

--- src/public/index.php
+++ src/public/index.php
@@ -5,12 +5,13 @@
  */
 
 date_default_timezone_set('Europe/Warsaw');
+
+define('APPLICATION_ENV_PROD', 'production');
+define('APPLICATION_ENV_DEV', 'development');
 define("APPLICATION_MICROTIME_START", microtime(true));
-
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
 define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+
 chdir(APPLICATION_PATH);
 
 require_once __DIR__ . '/../../vendor/autoload.php';
